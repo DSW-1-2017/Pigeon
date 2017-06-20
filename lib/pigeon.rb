72a1b80119d8f9bc861c420e792dd060bee29fa9
wsickert <wesley.sickert@gmail.com>
2017-06-20 16:53:17 -0300

    create simple communication

--- lib/pigeon.rb
+++ lib/pigeon.rb
@@ -2,101 +2,111 @@ module Pigeon
 	require 'bunny'
 
 	module Connection
-		# Create Template Method
-		def self.create_connection(hostname)
+		def start(hostname)
 			@connection = Bunny.new(hostname: hostname)
 	    	@connection.start
 		end
 
-		def self.create_channel
-			@channel = @connection.create_channel
+		def close
+			@connection.close
 		end
 	end
 
-	# Interface
-	class MessageCommand
-	  def send(message, receive)
-	    raise NotImplementedError, 'method not implemented'
-	  end
+	# Context of strategies.
+	class ProducerCommunication
+		attr_accessor :simple
+
+		def initialize(communication, hostname)
+			case communication
+			when :simple
+				@simple = Pigeon::SimpleProducer.new(hostname)
+			when :pubsub
+			end
+		end
+
+		def send(message, queue)
+			@simple.send(message, queue)
+		end
 	end
 
-	# This class has invoker of sender methods and has controll of executation
-	class MessageInvoker
-	  attr_accessor :interface_message
-	  # message_type = [:simple, :pubsub]
+	class ConsumerCommunication
+		attr_accessor :simple
+		attr_accessor :queue
 
-	  def initialize
-	    @interface_message = []
-	  end
+		def initialize(communication, hostname)
+			case communication
+			when :simple
+				@simple = Pigeon::SimpleProducer.new(hostname)
+			when :pubsub
+			end
+		end
 
-	  def send
-	    @interface_message.each do |interface|
-	      interface[:type].send(interface[:message], interface[:receive])
-	    end
-	  end
-	  def add_message(message, receive, type)
-
-	    case type
-		    when :simple
-		      hash = { type: Simple.new, message: message, receive: receive }
-		    when :pubsub
-		      hash = { type: PubSub.new, message: message, receive: receive }
-		    else
-		      raise NotImplementedError, 'type not implemented'
-	    end
-
-	    interface_message.push(hash)
-	  end
+		def listen_queue(queue)
+			@queue = @simple.channel.queue(queue)
+		end
 	end
 
-	# This class implements simple connection mode
-	class Simple < MessageCommand
-	  def connect(receive)
-	    @connection = Bunny.new(hostname: receive.ip_address)
-	    @connection.start
-	    puts "connect simple #{receive.ip_address}"
-	  end
-
-	  def send(message, receive)
-	    connect(receive)
-	    channel = @connection.create_channel
-	    receive.send_simple(message, channel)
-	    @connection.close
-	  end
-	end
-
-	# This class implements PubSub connection mode
-	class PubSub < MessageCommand
-	  def connect(receive)
-	    puts "connect pub_sub #{receive.ip_address}"
-	  end
-
-	  def send(message, receive)
-	    connect(receive)
-	    receive.send_pubsub(message)
-	  end
-	end
-
-	# This class contain configuration of receive and implementation methods
-	# of communication
-	class Receiver
+	# Abstract Strategies.
+	class ProducerStrategy
 		include Connection
-	  attr_accessor :host
-	  attr_accessor :call_name
+		attr_accessor :connection, :channel
 
-	  # def initialize(call_name, host)
-	  #   @call_name = call_name
-	  #   @host = host
-	  # end
+		def initialize(hostname)
+			@connection = self.start hostname
+			@channel = @connection.create_channel
+		end
 
-	  def send_simple(message, queue)
-	    queue = channel.queue(queue)
-	    channel.default_exchange.publish(message, routing_key: queue.name)
-	    puts "send simple '#{message}'"
-	  end
+		def send(message, queue)
+			raise NotImplementedError
+		end
 
-	  def send_pubsub(message)
-	    puts "send pub_sub '#{message}'"
-	  end
+		def create_queue(queue)
+			raise NotImplementedError
+		end
+	end
+
+	class ConsumerStrategy
+		include Connection
+		attr_accessor :connection, :channel
+
+		def initialize(hostname)
+			@connection = self.start hostname
+			@channel = @connection.create_channel
+		end
+
+		def listen_queue(queue)
+			raise NotImplementedError
+		end
+	end
+
+	# Concrete Producer Strategies.
+	class SimpleProducer < ProducerStrategy
+		
+		def initialize(hostname)
+			super
+		end
+
+		def send(message, queue)
+			queue = @channel.queue(queue)
+			@channel.default_exchange.publish(message, routing_key: queue.name)
+		end
+	end
+
+	class PubSubProducer < ProducerStrategy
+	end
+
+	# Concrete Consumer/Receiver Strategies
+	class SimpleConsumer < ConsumerStrategy
+		
+		def initialize(hostname)
+			super
+		end
+
+		def listen_queue(queue)
+			@channel.queue(queue)
+		end
+	end
+
+	class PubSubConsumer< ConsumerStrategy
 	end
 end
