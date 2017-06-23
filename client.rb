2d489641cf97729903df602284cf6ed0632865f6
Luan Guimarães <livreluan@gmail.com>
2017-06-23 01:08:01 -0300

    Creating basic client and server to test Pigeon

--- client.rb
+++ client.rb
@@ -2,6 +2,8 @@ require_relative 'lib/pigeon.rb'
 
 #Example
 #You only need to change :simple to :pubsub in client.rb and server.rb
-producer = Pigeon::ProducerCommunication.new(:simple, 'localhost')
+producer = Pigeon::ProducerCommunication.new(:rpc, 'localhost')
 producer.communicator.setup('x')
-producer.communicator.send("hello")
+producer.communicator.send("hello") do |response|
+  puts response
+end
--- server.rb
+++ server.rb
@@ -2,8 +2,9 @@ require_relative 'lib/pigeon.rb'
 
 #Example
 #You only need to change :simple to :pubsub in client.rb and server.rb
-receiver = Pigeon::ConsumerCommunication.new(:simple, 'localhost')
+receiver = Pigeon::ConsumerCommunication.new(:rpc, 'localhost')
 receiver.communicator.listen('x')
 receiver.communicator.subscribe do |info, properties, body|
   puts "[X] #{body}"
+  "Server's response!"
 end
